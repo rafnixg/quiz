8e26be912489a1e84a379783bd234734fc7366b2
rafnixg <rafnixg@gmail.com>
2015-07-12 19:33:04 -0430

    Creando Creditos

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
 
+/*GET Author */
+
+router.get('/author',function(req,res){
+	res.render('author')
+});
+
 module.exports = router;
